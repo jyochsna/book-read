597a652042ea4a87984240f8d4eddcd50cfeacd2
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-04-18 12:40:32 -0700

    in server js file process.env.mongodb_uri is edited

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://reactbook1:reactbook1@ds235947.mlab.com:35947/heroku_dgtk1843");
+mongoose.connect(//process.env.MONGODB_URI ||
+   "mongodb://user:user123@ds235947.mlab.com:35947/heroku_dgtk1843");
 
 // Start the API server
 app.listen(PORT, function() {
